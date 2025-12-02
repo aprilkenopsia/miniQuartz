5afc2068e880259b0079ccbf228f67a9f253e492
Zipra <zippyzipra@gmail.com>
2025-12-02 11:14:53 -0500

    - Song list, empty side panel, placeholder songs.

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,6 @@
+//use std::collections::{binary_heap::{IntoIter, Iter}, hash_map::Iter};
+use egui::ScrollArea;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -7,6 +10,12 @@ pub struct TemplateApp {
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+
+    #[serde(skip)] // Opting out of serialization needs this thing above it. What's serde?
+    songs: Songs, // I think this is basically doing what the 2021: 7a Rust tutorial said, but fits into the template given by egui. Probably better to do it like this? Feels cleaner.
+
+    #[serde(skip)]
+    row_height: Option<f32>,
 }
 
 impl Default for TemplateApp {
@@ -15,6 +24,9 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            // Not example stuff:
+            songs: Songs::new(),
+            row_height: None,
         }
     }
 }
@@ -31,6 +43,29 @@ impl TemplateApp {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
         } else {
             Default::default()
+        }
+    }
+}
+
+struct Songs {
+    articles: Vec<SongCardData>
+}
+
+struct SongCardData{
+    title: String,
+    artist: String,
+    length: String
+}
+
+impl Songs{
+    fn new() -> Songs{
+        let iter = (0..2000).map(|a| SongCardData{
+            title: (a).to_string(),
+            artist: (a+1).to_string(),
+            length: (a*2).to_string()
+        });
+        Songs{
+            articles: Vec::from_iter(iter)
         }
     }
 }
@@ -65,26 +100,57 @@ impl eframe::App for TemplateApp {
             });
         });
 
+        egui::SidePanel::left("playlists")
+        .resizable(true)
+        .default_width(100.0)
+        .min_width(20.0)
+        .show(ctx, |ui|{
+            ScrollArea::vertical().show(ui, |ui|{
+                ui.set_min_width(ui.available_width());
+                ui.label("Playlists Hereeeeeeeeeee");
+            });
+        });
+
+        egui::TopBottomPanel::bottom("status")
+        .resizable(true)
+        .min_height(30.0)
+        .max_height(500.0)
+        .show(ctx, |ui|{
+            ui.label("Status hereeee!!!");
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
+            ui.heading("miniQuartz");
+            let available_width = ui.available_width();
+            let height = self.row_height.unwrap_or(30.0);
+            let total_rows = self.songs.articles.len();
+            ScrollArea::vertical()
+            .max_width(available_width)
+            .show_rows(ui,height,self.songs.articles.len(),|ui, row_range|{
+                ui.label(available_width.to_string());
+                let buffer = 5;
+                let start = row_range.start.saturating_sub(buffer);
+                ui.label(start.to_string());
+                let end = (row_range.end + buffer).min(total_rows);
+                ui.label(end.to_string());
+                let buffered_range = start..end;
+                for i in buffered_range{ 
+                    let song = &self.songs.articles[i]; // Ampersand makes it read-only, since the for loop tries to own "articles"
+                    ui.set_min_width(available_width);
+                    ui.group(|ui|{
+                        ui.horizontal(|ui|{
+                            ui.label(egui::RichText::new(format!("Title: {}", song.title)).strong());
+                            ui.label(format!("Artist {}", song.artist));
+                            ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui|{
+                                ui.add_space(10.0);
+                                ui.label(format!("Length {}", song.length));
+                            });
+                        });
+                    });  
+                }
             });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+            
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 powered_by_egui_and_eframe(ui);
